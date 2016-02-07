01347c1d63af8a2d1cd119eec9754ace5d16bd7a
jerridan <jerridan@gmail.com>
2016-02-07 16:50:09 -0500

    First commit; created Makefile and new client file

---
+++ client/client.c
@@ -0,0 +1,14 @@
+/****************************************************************************
+ * client.c
+ *
+ * Computer Science 3305b - Spring 2016
+ * Author: Jerridan Quiring
+ *
+ * Implements a Unix-like terminal
+****************************************************************************/
+
+#include "client.h"
+
+int main() {
+  exit(EXIT_SUCCESS);
+}
---
+++ client/client.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ * client.h
+ *
+ * Computer Science 3305b - Spring 2016
+ * Author: Jerridan Quiring
+ *
+ * Implements a Unix-like terminal
+****************************************************************************/
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main();
+
+#endif
